a177e69bde3a90ef0a48e6727e7e8a17dd806d09
Drew_Keller <superior.drewology.gmail.com>
2022-02-06 20:52:06 -0500

    Base Layout made

--- src/components/LayoutComponent/LayoutComponent.jsx
+++ src/components/LayoutComponent/LayoutComponent.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Toolbar } from '@mui/material';
+
 
 const LayoutComponent = ({props}) => {
 
     return(
-        <div>Stuff</div>
+        <Box>
+        <AppBar position="static">
+            <Typography align='center' variant='h3'>Measurement Converter</Typography>
+        </AppBar>
+        </Box>
     );
 }
 
